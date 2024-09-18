42adbd9ad406d963be4f6750106d74157e5c94f9
rainboowbee <rainboowbee@gmail.com>
2024-09-18 23:50:07 +0300

    init app

---
+++ layots/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+useHead({
+  script: [{ src: "https://telegram.org/js/telegram-web-app.js", defer: true }],
+});
+</script>
+
+<template>
+  <div>
+    <main>
+      <ClientOnly>
+        <slot />
+      </ClientOnly>
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
